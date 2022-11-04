afbea156e40ee0a880d6a6669ecfa84af2c6dfc9
Sandro Hanea <sandrohanea@microsoft.com>
2022-11-04 14:20:08 +0100

    Added initial files

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public static void Test()
+        {
+
+        }
+    }
+}
---
+++ TestProject/Class1.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleApp3;
+using NUnit.Framework;
+
+namespace TestProject
+{
+    public class TestClass
+    {
+        [Test]
+        public void Test()
+        {
+            Program.Test();
+        }
+
+        [Test]
+        public void Test2()
+        {
+
+        }
+    }
+}
